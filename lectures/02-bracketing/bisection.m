f25b6df9da413ea61712924f519e00b12c589eda
Xavier Sánchez <xaronaxaxaxa@gmail.com>
2020-03-31 03:38:19 -0600

    Updated with many files for lecture 2

---
+++ lectures/02-bracketing/bisection.m
@@ -0,0 +1,23 @@
+function x_r = bisection(x_l, x_u, x_r, treshold)
+    f_l = f(x_l);
+    error = 1;
+    
+    while error > treshold
+        x_old = x_r;
+        x_r = (x_l + x_u) / 2;
+        f_r = f(x_r);
+        
+        error = abs((x_old - x_r) / x_old);
+        
+        test = f_l * f_r;
+        
+        if test < 0
+            x_u = x_r;
+        elseif test > 0
+            x_l = x_r;
+            f_l = f_r;
+        else
+            error = 0;
+        end
+    end
+end
---
+++ lectures/02-bracketing/f.m
@@ -0,0 +1,3 @@
+function y = f(x)
+y = x^2 + 10 * sin(x);
+end
